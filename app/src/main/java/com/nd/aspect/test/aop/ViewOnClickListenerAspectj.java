4641f10118f1956355f4d3b6fde283c5ad2dac31
薛文建 <wenjian133@163.com>
2017-09-11 10:03:26 +0800

    代码整理

--- app/src/main/java/com/nd/aspect/test/aop/ViewOnClickListenerAspectj.java
+++ app/src/main/java/com/nd/aspect/test/aop/ViewOnClickListenerAspectj.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by Xuewenjian on 2017/8/9 0009.
@@ -14,15 +15,19 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class ViewOnClickListenerAspectj {
     private static final String TAG = "ViewOnClickListenerAj";
+
+    @Pointcut("execution(* android.view.View.OnClickListener.onClick(android.view.View))")
+    public void pointcutOnClickListener() {}
+
     /**
      * android.view.View.OnClickListener.onClick(android.view.View)
      *
      * @param joinPoint JoinPoint
      * @throws Throwable Exception
      */
-    @Before("execution(* android.view.View.OnClickListener.onClick(android.view.View))")
-    public void onViewClickAOPB(final JoinPoint joinPoint) throws Throwable {
-        Log.d(TAG,"ViewOnClickListenerAspectj onViewClickAOPB 方法，在onClick之 前 被调用");
+    @Before("pointcutOnClickListener()")
+    public void beforePcOnClickListener(final JoinPoint joinPoint) throws Throwable {
+        Log.d(TAG,"ViewOnClickListenerAspectj beforePcOnClickListener 方法，在onClick之 前 被调用");
         if (joinPoint == null || joinPoint.getArgs() == null || joinPoint.getArgs().length != 1) {
             return ;
         }
@@ -33,13 +38,13 @@ public class ViewOnClickListenerAspectj {
         }
         if (view.getId() != View.NO_ID) {
             String idString = view.getContext().getResources().getResourceEntryName(view.getId());
-            Log.d(TAG,"ViewOnClickListenerAspectj onViewClickAOPB 方法，被点击的View id:"+idString);
+            Log.d(TAG,"ViewOnClickListenerAspectj beforePcOnClickListener 方法，被点击的View id:"+idString);
         }
-
     }
-    @After("execution(* android.view.View.OnClickListener.onClick(android.view.View))")
-    public void onViewClickAOP(final JoinPoint joinPoint) throws Throwable {
-        Log.d(TAG,"ViewOnClickListenerAspectj onViewClickAOP 方法，在onClick之 后 被调用");
+
+    @After("pointcutOnClickListener()")
+    public void afterPcOnClickListener(final JoinPoint joinPoint) throws Throwable {
+        Log.d(TAG,"ViewOnClickListenerAspectj afterPcOnClickListener 方法，在onClick之 后 被调用");
         //获取被点击的 View
         View view = (View) joinPoint.getArgs()[0];
         if (view == null) {
@@ -47,7 +52,7 @@ public class ViewOnClickListenerAspectj {
         }
         if (view.getId() != View.NO_ID) {
             String idString = view.getContext().getResources().getResourceEntryName(view.getId());
-            Log.d(TAG,"ViewOnClickListenerAspectj onViewClickAOP 方法，被点击的View id:"+idString);
+            Log.d(TAG,"ViewOnClickListenerAspectj afterPcOnClickListener 方法，被点击的View id:"+idString);
         }
     }
 
